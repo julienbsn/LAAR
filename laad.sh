c7f13eca9da8ba2cea1b3da3b795a0b38e817944
julienbsn <julienbsn@protonmail.com>
2018-01-31 18:43:00 +0100

    Fix: * gpg keys in laad.sh

--- laad.sh
+++ laad.sh
@@ -12,54 +12,79 @@ fi
 # check required packages
 req_pkgs=(curl gnupg apt-transport-https)
 for req_pkg in "${req_pkgs[@]}"; do
-  if [ dpkg -s $req_pkg ]; then
+  if [[ `dpkg -s $req_pkg` ]]; then
     echo "required package $req_pkg found!"
   else
     echo "required package $req_pkg not found! Installing ..."
-    apt install $req_pkg
+    apt install -y $req_pkg
   fi
 done
 
 # add contrib, non-free, CISOfy, NodeJS, atom.io && slack repositories
 contrib_non_free="contrib non-free"
-ciso_repo_gpg="https://packages.cisofy.com/keys/cisofy-software-public.key | apt-key add -"
+ciso_repo_gpg="https://packages.cisofy.com/keys/cisofy-software-public.key"
 ciso_repo="deb https://packages.cisofy.com/community/lynis/deb/ stable main"
-nodejs_8_repo="https://deb.nodesource.com/setup_8.x | bash -"
-nodejs_9_repo="https://deb.nodesource.com/setup_9.x | bash -"
-atom_repo_gpg="https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -"
+nodejs_8_repo="https://deb.nodesource.com/setup_8.x"
+nodejs_9_repo="https://deb.nodesource.com/setup_9.x"
+nodejs_repo_version="node -v"
+atom_repo_gpg="https://packagecloud.io/AtomEditor/atom/gpgkey"
 atom_repo="deb https://packagecloud.io/AtomEditor/atom/any/ any main"
-slacktechnologies_repo_gpg="https://packagecloud.io/slacktechnologies/slack/gpgkey | apt-key add -"
+slacktechnologies_repo_gpg="https://packagecloud.io/slacktechnologies/slack/gpgkey"
 slacktechnologies_repo="deb https://packagecloud.io/slacktechnologies/slack/debian/ jessie main"
 
-if [ -f /etc/apt/sources.list -eq 0 ]; then
+if [[ -f /etc/apt/sources.list ]]; then
   # explore file and if string notpresent; then add repo
   echo "enter in add repo condition"
-  if [ grep "contrib non-free" /etc/apt/sources.list ]; then
+  if [[ `grep 'contrib non-free' /etc/apt/sources.list` ]]; then
     #statements contrib non-free
-    echo "'contrib' && 'non-free' repositories found!"
+    echo "contrib && non-free repositories found!"
   else
-    sed -i '/buster/ s/$/ "${contrib_non_free}"/' /etc/apt/sources.list
+    echo "'contrib' && 'non-free' repositories not found! Adding ..."
+    sed -i "/buster/ s/$/ $contrib_non_free/" /etc/apt/sources.list
   fi
 
-  if [ grep "cisofy" /etc/apt/sources.list ]; then
+  if [[ `grep 'cisofy' /etc/apt/sources.list` ]]; then
     echo "CISOfy repository found!"
   else
-    curl -sL "${ciso_repo_gpg}" &> /dev/null
-    echo "${ciso_repo}" >> /etc/apt/sources.list
+    echo "CISOfy repository not found! Adding ..."
+    curl -sL $ciso_repo_gpg | apt-key add - &> /dev/null
+    echo $ciso_repo >> /etc/apt/sources.list
   fi
 
-  if [ grep "AtomEditor" /etc/apt/sources.list ]; then
+  if [[ `grep 'AtomEditor' /etc/apt/sources.list` ]]; then
     echo "Atom.io repository found!"
   else
-    curl -sL "${atom_repo_gpg}" &> /dev/null
-    echo "${atom_repo}" >> /etc/apt/sources.list
+    echo "Atom.io repository not found! Adding ..."
+    curl -sL $atom_repo_gpg | apt-key add - &> /dev/null
+    echo $atom_repo >> /etc/apt/sources.list
   fi
 
-  if [ grep "slacktechnologies" /etc/apt/sources.list ]; then
+  if [[ `grep 'slacktechnologies' /etc/apt/sources.list` ]]; then
     echo "Slack Technologies repository found!"
   else
-    curl -sL "${slacktechnologies_repo_gpg}" &> /dev/null
-    echo "${slacktechnologies_repo}" >> /etc/apt/sources.list
+    echo "Slack Technologies repository not found! Adding ..."
+    curl -sL $slacktechnologies_repo_gpg | apt-key add - &> /dev/null
+    echo $slacktechnologies_repo >> /etc/apt/sources.list
+  fi
+
+  if [[ -f /etc/apt/sources.list.d/nodesource.list ]]; then
+    echo "NodeJS repository found!"
+  else
+    echo "NodeJS repository not found! Adding ..."
+    printf "Which version do you want to use? [8.x/9.x] : "
+    read -r node_version
+    case $node_version in
+      "8.x" )
+        curl -sL $nodejs_8_repo | bash - &> /dev/null
+        ;;
+      "9.x" )
+        curl -sL $nodejs_9_repo | bash - &> /dev/null
+        ;;
+      *)
+        echo "Nothing was selected ..."
+        ;;
+    esac
+    echo "NodeJS repository added! version is : $node_version"
   fi
 
 else
